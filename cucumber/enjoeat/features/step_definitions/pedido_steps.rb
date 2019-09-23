6efbc2f26302dcc9a9c2b9eea48f5fe915562335
Jilvan Pereira <jilvan.pereira@gold360.local>
2019-09-23 15:08:40 -0300

    Aula 20 - Ferrari ou Fusca? BDD ou BTD? || PART II

--- cucumber/enjoeat/features/step_definitions/pedido_steps.rb
+++ cucumber/enjoeat/features/step_definitions/pedido_steps.rb
@@ -10,13 +10,13 @@ end
 Dado("informei os meus dados de entrega:") do |table|
     user = table.rows_hash
     @order_page.fill_user_date(user)
+end
+  
+Quando("eu finalizo o pedido com {string}") do |payment|
+    @order_page.checkout(payment)
     sleep 3
 end
   
-Quando("eu finalizo o pedido com {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-Então("devo ver a seguinte mensagem:") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+Então("devo ver a seguinte mensagem:") do |expect_message|
+    expect(@order_page.sumary).to have_text expect_message
 end
